8128ee2d2d5402182ddc2bb8fd8bf1b875b595ee
tejodhara <tejodhara@gmail.com>
2022-08-02 12:12:52 +0530

    mongoose

--- mongoos/index.js
+++ mongoos/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 
 // connect method DB url, keys
 
-mongoose.connect('mongodb://localhost:27017/demo',{})
+mongoose
+    .connect('mongodb://localhost:27017/demo',{})               // to connect to the database
     .then(()=>{
         console.log(`Database is connected Sucssesfully`);
     })
@@ -11,18 +12,33 @@ mongoose.connect('mongodb://localhost:27017/demo',{})
         console.log(err);
     })
 
+// in schema we need to give keys and datatype 
+let person= mongoose.Schema({
+                name: String,
+                address: {
+                    city:String,
+                    street: String,
+                    pin: Number,
+                },
+                phoneNumber: Number,
+                date:{
+                    type: Date,
+                    default: Date.now()     // date will give current date
+                }
+            });
 
+// model is a class
+const Employee = new mongoose.model('employee',person)         // it takes two arguments is collection name and Schema
 
-
-
-
-
-
-
-
-
-
-// const app = express()
-// const port = 3007
-
-// app.listen(port,console.log(`Server is listening to port ${port}`))
+// insert data in colleciton
+const person1 = new Employee({
+    name:'Teja',
+    addrss: {
+        city:'bangalore',
+        street:'BTM',
+        pin:563130,
+    },
+    phoneNumber:8247802033,
+    date: new Date()
+})
+person1.save();         // it will save the data in the compass
